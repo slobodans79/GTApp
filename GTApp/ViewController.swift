8fa800068f5e94814059fbe0f0fe082f5c577aa0
Slobodan Stamenic <slobodan.s@connectik.com>
2019-08-13 16:33:55 +0200

    initial commit

---
+++ GTApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GTApp
+//
+//  Created by Slobodan Stamenic on 8/13/19.
+//  Copyright © 2019 Slobodan Stamenic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
